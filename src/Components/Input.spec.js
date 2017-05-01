abce6ab03963db95b89c5c51c0c8b8eebc1720a7
Michael Smith <Michael.Smith@arcadiagroup.co.uk>
2017-05-01 17:09:49 +0100

    Moves Input component tests to jest

---
+++ src/Components/Input.spec.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Input from './Input'
+
+describe('<Input/>', () => {
+  it('has an input for number of people and number of slices', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find('input[type="number"]').length).toBe(2)
+  })
+  it('raises an event when the number of people value changes', () => {
+    const onChange = jest.fn()
+    const wrapper = shallow(<Input handlePeopleChange={onChange} />)
+    wrapper
+      .find('#number-of-people')
+      .first()
+      .simulate('change', { target: { value: '7' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+  it('raises an event when the number of slices value changes', () => {
+    const onChange = jest.fn()
+    const wrapper = shallow(<Input handleSlicesChange={onChange} />)
+    wrapper
+      .find('#number-of-slices')
+      .first()
+      .simulate('change', { target: { value: '7' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+  it('raises an event when the bogof value changes', () => {
+    const onChange = jest.fn()
+    const wrapper = shallow(<Input handleBogofChange={onChange} />)
+    wrapper
+      .find('#bogof')
+      .first()
+      .simulate('change', { target: { value: 'true' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+})
