874756a5eabbfe24fc5156bac62d83e4751e1add
Michael Smith <Michael.Smith@arcadiagroup.co.uk>
2017-05-01 13:16:59 +0100

    Moves pizza calculator tests over to jest

---
+++ src/pizzaCalculator.spec.js
@@ -0,0 +1,39 @@
+import pizzaCalculator from '../src/pizzaCalculator'
+
+const helper = (pizzaArgs) => (
+  pizzaCalculator(pizzaArgs).pizzas
+)
+describe('Pizza Calculator', () => {
+  it('Should return an object representing the amount of Pizza needed', () => {
+    expect(helper({ slicesNeeded: 0 })).toEqual({
+      large: 0,
+      medium: 0
+    })
+    expect(helper({ slicesNeeded: 12 })).toEqual({
+      large: 0,
+      medium: 2
+    })
+    expect(helper({ slicesNeeded: 42 })).toEqual({
+      large: 1,
+      medium: 4
+    })
+    expect(helper({ slicesNeeded: 98 })).toEqual({
+      large: 9,
+      medium: 1
+    })
+  })
+  it('Should return an object representing the amount of Pizza needed when bogof offer is on', () => {
+    expect(
+      helper({ slicesNeeded: 0, bogof: true })
+    ).toEqual({ large: 0, medium: 0 })
+    expect(
+      helper({ slicesNeeded: 12, bogof: true })
+    ).toEqual({ large: 0, medium: 2 })
+    expect(
+      helper({ slicesNeeded: 42, bogof: true })
+    ).toEqual({ large: 0, medium: 6 })
+    expect(
+      helper({ slicesNeeded: 90, bogof: true })
+    ).toEqual({ large: 6, medium: 4 })
+  })
+})
