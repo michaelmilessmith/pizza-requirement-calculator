30d21bb67fb48d70474da7fd3ce07a63841bf8db
Michael Smith <Michael.Smith@arcadiagroup.co.uk>
2017-06-07 11:55:20 +0100

    Improves pizza calcultor function once again, can now hadle very big numbers

--- src/pizzaCalculator.js
+++ src/pizzaCalculator.js
@@ -1,34 +1,41 @@
 // @flow
 
-const mediumSlices = 8
-const largeSlices = 10
-
-const calculateSlices = ({ medium, large }) => {
-  return medium * mediumSlices + large * largeSlices
-}
-
 type PizzaOptions = {
   slicesNeeded: number,
   bogof: boolean
 }
 
 const pizzaCalculator = ({ slicesNeeded, bogof }: PizzaOptions) => {
-  const toChange = bogof ? 2 : 1
-  let total = 0
-  const pizzas = { large: 0, medium: 0 }
-  let numberOfPizzas = 0
-  while (slicesNeeded > total) {
-    if (pizzas.large === numberOfPizzas) {
-      numberOfPizzas += toChange
-      pizzas.medium = numberOfPizzas
-      pizzas.large = 0
-    } else {
-      pizzas.medium -= toChange
-      pizzas.large += toChange
+  const isWhole = n => n % 1 === 0
+  const doubleIfBogof = value => (bogof ? 2 * value : value)
+  const largeSlice = doubleIfBogof(10)
+  const mediumSlice = doubleIfBogof(8)
+  const solveMedium = (large, slices) =>
+    (-largeSlice * large + slices) / mediumSlice
+  const solveLarge = (slices, noOfPizzas) =>
+    (slices - mediumSlice * noOfPizzas) / (largeSlice - mediumSlice)
+
+  for (
+    let slices = slicesNeeded;
+    slices < slicesNeeded + largeSlice;
+    slices++
+  ) {
+    let noOfPizzas = Math.floor(slices / largeSlice) - 1
+    let noOfLarges = noOfPizzas + 1
+    for (; noOfLarges > noOfPizzas - 1; noOfPizzas++) {
+      noOfLarges = solveLarge(slices, noOfPizzas)
     }
-    total = calculateSlices(pizzas)
+    if (noOfLarges >= 0 && isWhole(noOfLarges)) {
+      const medium = solveMedium(noOfLarges, slices)
+      const pizzas = {
+        large: doubleIfBogof(noOfLarges),
+        medium: doubleIfBogof(medium)
+      }
+      const total = slices
+      return { pizzas, total }
+    }
   }
-  return { pizzas, total }
+  return { pizzas: { large: 0, medium: 0 }, total: 0 }
 }
 
 export default pizzaCalculator
